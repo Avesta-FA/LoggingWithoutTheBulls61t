e3fea23d5428d62fa8985c061733ff027a16b0fb
Avesta <58849566+Avesta-FA@users.noreply.github.com>
2022-07-31 14:38:44 +0430

    Add files via upload

---
+++ Windows/Enable-Logging.ps1
@@ -0,0 +1,180 @@
+# No guarantees, no warranties, and no right reserved.
+#
+# This script is intended to be a simple way for cyber security engineers to enable tactical
+# logging on their windows endpoints.
+#
+# I avesta have only gathered all the open source information and have grouped it all into one script.
+#
+# You only need to run the script and enjoy all the logs.
+# 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+############################
+#
+#
+#	Installing Sysmon
+#
+#
+############################
+# You will need to download sysmon and add it inside the same folder.
+# You will also need to add SwiftOnSecurity sysmon config file.
+
+
+New-Item -ItemType Directory -Path c:\sysmon -ErrorAction Stop
+Copy-Item -Path ".\Sysmon64.exe" -Destination c:\sysmon -ErrorAction Stop
+Copy-Item -Path ".\sysmonconfig-export.xml" -Destination c:\sysmon -ErrorAction Stop
+
+
+"c:\sysmon\Sysmon64.exe" /accepteula -i c:\sysmon\sysmonconfig-export.xml -ErrorAction Stop
+
+net start Sysmon64 
+
+
+
+############################
+#
+#
+#	Enabling Script block logging
+#
+#
+############################
+# For more powershell logging you can also add transcript logging. 
+#You can also find this on the Microsoft docs.
+$basePath = 'HKLM:\Software\Policies\Microsoft\Windows' +
+      '\PowerShell\ScriptBlockLogging'
+
+    if(-not (Test-Path $basePath))
+    {
+        $null = New-Item $basePath -Force
+    }
+
+    Set-ItemProperty $basePath -Name EnableScriptBlockLogging -Value "1"
+
+
+
+############################
+#
+#
+#	Adding Advanced Audit policies
+#
+#
+############################
+
+
+
+
+
+auditpol /set /subcategory:"Filtering Platform Policy Change" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"MPSSVC Rule-Level Policy Change" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"File System" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Detailed File Share" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Filtering Platform Connection" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Registry" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"File Share" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Certification Services" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"SAM" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Other Logon/Logoff Events" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Credential Validation" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Other Account Management Events" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"User Account Management" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Plug and Play Events" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Directory Service Access" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Directory Service Changes" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Account Lockout" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Group Membership" /Success:Enable 
+
+auditpol /set /subcategory:"Removable Storage" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Policy Change" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Authentication Policy Change" /Success:Enable 
+
+auditpol /set /subcategory:"Authorization Policy Change" /Success:Enable 
+
+auditpol /set /subcategory:"Sensitive Privilege Use" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"IPSec Driver" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Security State Change" /Success:Enable 
+
+
+auditpol /set /subcategory:"Other System Events" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Security System Extension" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"System Integrity" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"computer account management" /Success:Enable 
+
+auditpol /set /subcategory:"audit policy change" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"security group management" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"logoff" /Success:Enable 
+
+auditpol /set /subcategory:"logon" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"security group management" /Success:Enable /Failure:Enable
+
+auditpol /set /subcategory:"Special Logon" /Success:Enable 
+
+auditpol /set /subcategory:"Process Creation" /Success:Enable 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
