0549903cb27e2bb230f6d083976c2de229d84d66
Avesta <58849566+Avesta-FA@users.noreply.github.com>
2022-08-10 16:28:53 +0430

    Update increase-Logging.ps1

--- Windows/increase-Logging.ps1
+++ Windows/increase-Logging.ps1
@@ -1,19 +1,15 @@
 #Author: Avesta Fahimipour 
-#
 #No guarantees, no warranties, and no rights reserved.
-#
-#
-#
 #It is good practice to increase the size of the event logs.
 #
 #
 #
 #
 #
-
-Limit-EventLog -LogName System -MaximumSize 2GB
-Limit-EventLog -LogName Security -MaximumSize 2GB
-Limit-EventLog -LogName Application -MaximumSize 2GB
+#
+Limit-EventLog -LogName 'System' -MaximumSize 2GB
+Limit-EventLog -LogName 'Security' -MaximumSize 2GB
+Limit-EventLog -LogName 'Application' -MaximumSize 2GB
 Limit-EventLog -LogName 'Windows PowerShell' -MaximumSize 2GB
 
 # Feel free to add more.
